d2a209d7180ae31a0c737dcc62e929f4edf411b9
Ankit Singh <ankits19783@gmail.com>
2026-05-05 18:54:09 +0530

    WEBSITE READY

---
+++ value.js
@@ -0,0 +1,10 @@
+let imgwhy = document.querySelector("#why");
+let imglove = document.querySelector("#love_too");
+function yes() {
+    imglove.style.display = "block";
+    imgwhy.style.display = "none";
+}
+function no() {
+    imgwhy.style.display = "block";
+    imglove.style.display = "none";
+}
